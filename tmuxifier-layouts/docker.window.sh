55ca62e4d049ab65367ecddebbe9b6a552cc8a40
Brian Clements <brian@brianclements.net>
2014-07-30 23:21:46 -0700

    Add tmuxifier
    -layouts, zsh settings and aliases
    -other minor zsh tweaks

---
+++ tmuxifier-layouts/docker.window.sh
@@ -0,0 +1,31 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+#window_root "~/Projects/docker"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "docker"
+
+# Split window into panes.
+run_cmd "watch sudo docker images"
+split_v 65
+run_cmd "watch sudo docker ps -a"
+split_v 50
+run_cmd "sudo true && i && clear"
+
+select_pane 1
+split_h 40
+run_cmd "sudo true && watch 'df -h | grep var'" 2
+
+select_pane 4
+
+# Run commands.
+#run_cmd "top"     # runs in active pane
+#run_cmd "date" 1  # runs in pane 1
+
+# Paste text
+#send_keys "top"    # paste into active pane
+#send_keys "date" 1 # paste into pane 1
+
+# Set active pane.
+# select_pane 2
